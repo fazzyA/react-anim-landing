6027c57497f63d8ef122dbd6e12960d51ffc5694
fazzyA <faz.pak@gmail.com>
2020-07-24 01:21:33 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Menu from './Navbar';
 import Home from './Home';
-import AnimatedHeading from './AnimatedHeading';
-import Rocket from './Rocket';
+// import AnimatedHeading from './AnimatedHeading';
+// import Rocket from './Rocket';
 import Section1 from './Section1';
 import { Container } from 'react-bootstrap';
 import Section2 from './Section2';
 import Footer from './Footer';
 import Client from './Client';
-import HeroImage from './HeroImage';
+// import HeroImage from './HeroImage';
 
 function App() {
   return (
